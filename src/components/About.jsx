5054512183901e8139766dd03397568d0d5c5094
Hvatov Maksim <1032204364@pfur.ru>
2023-05-23 12:26:16 +0300

    added cards

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,6 +6,29 @@ import {styles} from "../styels.js";
 import {services} from "../constants/index.js";
 import {fadeIn,textVariant} from "../utils/motion.js";
 
+
+const ServiceCard = ({title, index, icon}) => {
+    const tiltOptions = {
+        max: 45,
+        scale: 1,
+        speed: 450
+    }
+
+    return (
+        <Tilt className="xs:w-[250px] w-full">
+            <motion.div variants={fadeIn("right", "spring", 0.5*index, 0.75)}
+                        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card">
+
+                <div options={tiltOptions} className="bg-tertiary rounde-[20px] py-5 px-12
+                                                        min-h-[280px] flex justify-evenly items-center flex-col">
+                    <img src={icon} alt={title} className="w-16 h-16 object-contain"/>
+                    <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+                </div>
+
+            </motion.div>
+        </Tilt>
+    )
+}
 const About = () => {
   return (
       <>
@@ -23,6 +46,15 @@ const About = () => {
            easily can find the common language with the team. I want to work with you right now!
        </motion.p>
 
+          <div className="mt-20 flex flex-wrap gap-10">
+              {
+                  services.map((service ,index) => {
+                      return (
+                          <ServiceCard key={service.title} index={index} {...service}/>
+                      )
+                  })
+              }
+          </div>
 
       </>
   )
